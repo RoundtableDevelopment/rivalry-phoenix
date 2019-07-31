ffac150ce8703bbefd29fa534f29f010624ad0f7
dmcbrayer <dmcbrayer@gmail.com>
2019-07-30 22:17:35 -0400

    Adds accounts context and User module

--- src/config/test.exs
+++ src/config/test.exs
@@ -5,7 +5,7 @@ config :rivalry, Rivalry.Repo,
   username: "postgres",
   password: "postgres",
   database: "rivalry_test",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
---
+++ src/lib/rivalry/accounts/user.ex
@@ -0,0 +1,31 @@
+defmodule Rivalry.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
+    |> unique_constraint(:username)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Pbkdf2.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+end
