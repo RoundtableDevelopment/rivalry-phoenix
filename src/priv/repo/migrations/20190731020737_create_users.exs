acbd84d0de37a473781da1d4f817dc09520943e3
dmcbrayer <dmcbrayer@gmail.com>
2019-07-30 22:17:23 -0400

    Creates users table

---
+++ src/priv/repo/migrations/20190731020737_create_users.exs
@@ -0,0 +1,15 @@
+defmodule Rivalry.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string, null: false
+      add :email, :string
+      add :password_hash, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:username])
+  end
+end
